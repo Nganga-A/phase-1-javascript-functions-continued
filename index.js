a3b87f931a2bd202b856c295c9201ac3e069fb5e
Abed Nganga <ngangaabed@gmail.com>
2023-06-15 07:34:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+ const mondayWork = function(activity = "go to the office") {
+     return `This Monday, I will ${activity}.`;
+   };
+
+// const mondayWork = function(activity = "go to the office") {
+//     if (activity === "go to the office") {
+//       return "This Monday, I will go to the office.";
+//     } else {
+//       return `This Monday, I will ${activity}.`;
+//     }
+//   };
+
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+
